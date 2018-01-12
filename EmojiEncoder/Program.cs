cf9bc49040969c342b09dab760ff804f80037fd3
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-12 10:39:48 -0800

    Even larger outputs!

--- EmojiEncoder/Program.cs
+++ EmojiEncoder/Program.cs
@@ -29,8 +29,8 @@ namespace EmojiEncoder
                 FileStream outfs = File.Open(args[1], FileMode.Create, FileAccess.Write, FileShare.None);
                 BufferedStream outbs = new BufferedStream(outfs);
 
-                byte[] onebit = Encoding.UTF8.GetBytes("😂");
-                byte[] zerobit = Encoding.UTF8.GetBytes("😎");
+                byte[] onebit = Encoding.UTF8.GetBytes("👌🏻");
+                byte[] zerobit = Encoding.UTF8.GetBytes("🤙🏻");
                 int emojilen = onebit.Length;
                 
                 byte[] bytes = new byte[1];
